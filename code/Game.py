0a3c73aafddf34865345806f80943b7c0252807e
hermessantos14 <farias.hermes@gmail.com>
2026-03-03 19:05:58 -0300

    Initial loop menu

---
+++ code/Game.py
@@ -0,0 +1,20 @@
+import pygame
+
+from code.Menu import Menu
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(900, 600))
+
+    def run(self):
+
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+            # check for all events
+            # for event in pygame.event.get():
+            #   if event.type == pygame.QUIT:
+            #       pygame.quit()  # fecha a janela do jogo
+            #       quit()  # encerra o pygame
--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('Setup start')
-pygame.init()
-window = pygame.display.set_mode(size=(800, 600))
-
-print('Setup end')
-
-print('Loop start')
-while True:
-    #check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #fecha a janela do jogo
-            quit() #encerra o pygame
+game = Game()
+game.run()
